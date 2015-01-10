e9444a0e69d94048f3d8748481040a39fd3fc3ba
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-01-10 10:48:17 -0500

    Parsing resource files

--- pointrel.js
+++ pointrel.js
@@ -5,6 +5,8 @@
 
 "use strict";
 
+var resourcesDirectory = "resources";
+
 var command = process.argv[2];
 
 if (!command) {
@@ -29,9 +31,38 @@ if (command === "add") {
   }
   var contents = {command: "add", a: args[0], b: args[1], c: args[2]};
   var currentTimeInMillseconds = new Date().getTime()
-  var fileName = "pointrel_" + currentTimeInMillseconds + ".json";
+  var fileName = "resources/pointrel_" + currentTimeInMillseconds + ".json";
   var output = JSON.stringify(contents, null, 2) + "\n";
   fs.writeFileSync(fileName, output);
   process.exit(0);
+}
+
+function strStartsWith(str, prefix) {
+    return str.indexOf(prefix) === 0;
+}
+
+function strEndsWith(str, suffix) {
+    return str.match(suffix + "$") == suffix;
+}
+
+function readData()  {
+  var files = fs.readdirSync(resourcesDirectory);
+  for (var i in files) {
+    if (!files.hasOwnProperty(i)) continue;
+    var shortFileName = files[i];
+    var isResourceFile = strStartsWith(shortFileName, "pointrel_") && strEndsWith(shortFileName, ".json");
+    if (!isResourceFile) continue;  
+    var fullFileName = resourcesDirectory + '/' + shortFileName;
+    var data = JSON.parse(fs.readFileSync(fullFileName, 'utf8'));
+    console.log("read:", fullFileName, data);
+  }
+}
+
+if (command === "find") {
+  if (args.length !== 3) {
+    console.log("find command needs three args");
+    process.exit(-1);
+  }
+  readData();
 }
 
