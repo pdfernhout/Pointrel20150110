332dea039974c319f85c69c6f0903bb496cd4fab
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2016-05-30 19:04:51 -0400

    Add shell script to upload IBIS application

---
+++ demos/upload-ibis.sh
@@ -0,0 +1,24 @@
+# Commands to upload IBIS application
+p upload ibis/widgetSupport.js
+p upload ibis/ibis.html
+p upload ibis/CompendiumIcons/minus.png
+p upload ibis/CompendiumIcons/list.png
+p upload ibis/CompendiumIcons/trashbin.png
+p upload ibis/CompendiumIcons/note.png
+p upload ibis/CompendiumIcons/trashbinfull.png
+p upload ibis/CompendiumIcons/map.png
+p upload ibis/CompendiumIcons/argument.png
+p upload ibis/CompendiumIcons/position2.png
+p upload ibis/CompendiumIcons/decision-old.png
+p upload ibis/CompendiumIcons/plus.png
+p upload ibis/CompendiumIcons/reference.png
+p upload ibis/CompendiumIcons/decision.png
+p upload ibis/CompendiumIcons/position.png
+p upload ibis/CompendiumIcons/issue.png
+p upload ibis/ibis.js
+p upload ibis/CrystalClearIcons/Load.png
+p upload ibis/CrystalClearIcons/Import.png
+p upload ibis/CrystalClearIcons/Delete.png
+p upload ibis/CrystalClearIcons/LeftArrow.png
+p upload ibis/CrystalClearIcons/Edit.png
+p upload ibis/CrystalClearIcons/Save.png
